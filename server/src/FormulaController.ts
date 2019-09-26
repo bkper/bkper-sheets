6fe20f9cdbf033ca6337393f165649929f442077
mael <mael@bkper.com>
2019-09-26 09:17:53 -0300

    Refactoring to cache instead of updateCount

--- server/src/FormulaController.ts
+++ server/src/FormulaController.ts
@@ -2,43 +2,40 @@
  * Fetch Transactions
  *
  * @param {string} bookId The universal Book Id.
- * @param {number} updateCount Starts in 1 and increase on every update.
+ * @param {number} cache Increase to clean cache and force fetch update.
  * @param {string} query The transactions query.
  * 
  * @customfunction
  */
-function BKPER_TRANSACTIONS(bookId: string, updateCount: number, query: string): any[][] {
-  return BkperApp.getBook(bookId).createTransactionsDataTable(query).addUrls().build();
+function BKPER_TRANSACTIONS(bookId: string, cache: number, query: string): any[][] {
+  return BkperApp.getBook(bookId).createTransactionsDataTable(query).includeUrls(true).build();
 }
 
 /**
  * Fetch Total Balances
  * 
  * @param {string} bookId The universal Book Id.
- * @param {number} updateCount Starts in 1 and increase on every update.
+ * @param {number} cache Increase to clean cache and force fetch update.
  * @param {string} query The balances query.
  * @param {boolean=} expanded Expand group accounts
  * @param {boolean=} transposed Transpose the result
  * 
  * @customfunction
  */
-function BKPER_BALANCES_TOTAL(bookId: string, updateCount: number, query: string, expanded?: boolean, transposed?: boolean): any[][] {
+function BKPER_BALANCES_TOTAL(bookId: string, cache: number, query: string, expanded?: boolean, transposed?: boolean): any[][] {
   let balanceReport = BkperApp.getBook(bookId).getBalanceReport(query);
-  const builder = balanceReport.createDataTable().setBalanceType(BkperApp.BalanceType.TOTAL);
-  if (expanded) {
-    builder.expandGroups();
-  }
-  if (transposed) {
-    builder.transpose();
-  }
-  return builder.build();
+  return balanceReport.createDataTable()
+  .type(BkperApp.BalanceType.TOTAL)
+  .expanded(expanded)
+  .transposed(transposed)
+  .build();  
 }
 
 /**
  * Fetch Period Balances
  *
  * @param {string} bookId The universal Book Id.
- * @param {number} updateCount Starts in 1 and increase on every update.
+ * @param {number} cache Increase to clean cache and force fetch update.
  * @param {string} query The balances query.
  * @param {boolean=} expanded Expand group accounts
  * @param {boolean=} transposed Transpose the result
@@ -46,37 +43,31 @@ function BKPER_BALANCES_TOTAL(bookId: string, updateCount: number, query: string
  * 
  * @customfunction
  */
-function BKPER_BALANCES_PERIOD(bookId: string, updateCount: number, query: string, expanded?: boolean, transposed?: boolean): any[][] {
+function BKPER_BALANCES_PERIOD(bookId: string, cache: number, query: string, expanded?: boolean, transposed?: boolean): any[][] {
   let balanceReport = BkperApp.getBook(bookId).getBalanceReport(query);
-  const builder = balanceReport.createDataTable().setBalanceType(BkperApp.BalanceType.PERIOD);
-  if (expanded) {
-    builder.expandGroups();
-  }
-  if (transposed) {
-    builder.transpose();
-  }
-  return builder.build();
+  return balanceReport.createDataTable()
+  .type(BkperApp.BalanceType.PERIOD)
+  .expanded(expanded)
+  .transposed(transposed)
+  .build();
 }
 
 /**
  * Fetch Cumulative Balances
  * 
  * @param {string} bookId The universal Book Id.
- * @param {number} updateCount Starts in 1 and increase on every update.
+ * @param {number} cache Increase to clean cache and force fetch update.
  * @param {string} query The balances query.
  * @param {boolean=} expanded Expand group accounts
  * @param {boolean=} transposed Transpose the result
  * 
  * @customfunction
  */
-function BKPER_BALANCES_CUMULATIVE(bookId: string, updateCount: number, query: string, expanded?: boolean, transposed?: boolean): any[][] {
+function BKPER_BALANCES_CUMULATIVE(bookId: string, cache: number, query: string, expanded?: boolean, transposed?: boolean): any[][] {
   let balanceReport = BkperApp.getBook(bookId).getBalanceReport(query);
-  const builder = balanceReport.createDataTable().setBalanceType(BkperApp.BalanceType.CUMULATIVE);
-  if (expanded) {
-    builder.expandGroups();
-  }
-  if (transposed) {
-    builder.transpose();
-  }  
-  return builder.build();
+  return balanceReport.createDataTable()
+  .type(BkperApp.BalanceType.CUMULATIVE)
+  .expanded(expanded)
+  .transposed(transposed)
+  .build();
 }
