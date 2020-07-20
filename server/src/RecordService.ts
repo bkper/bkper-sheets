2816719a7561c9a10c55b4bd8042b3160fd71bf4
mael <mael@bkper.com>
2020-07-20 07:35:45 -0300

    Highlight recorded accounts rows

--- server/src/RecordService.ts
+++ server/src/RecordService.ts
@@ -24,9 +24,18 @@ namespace RecordService {
 
     let values = selectedRange.getValues();
 
+    let nameRowIndexMap: any = {};
+    let maxLength = 0;
+
     let groups: string[] = []
     for (let i = 0; i < values.length; i++) {
       const row = values[i]
+      const name = row[0] + '';
+      nameRowIndexMap[name] = i;
+      if (row.length >= maxLength) {
+        maxLength = row.length;
+      }
+
       if (row.length > 1) {
         for (let j = 1; j < row.length; j++) {
           const cell = row[j];
@@ -41,11 +50,47 @@ namespace RecordService {
       book.createGroups(groups);
     }
 
-    let createdAccounts = book.createAccounts(values);
+    book.createAccounts(values);
 
-    //TODO hightlight
-
+    if (highlight) {
+      let backgrounds: any[][] = initilizeMatrix(new Array(values.length), maxLength);
+      let accounts = book.getAccounts();
+      for (const account of accounts) {
+        const index = nameRowIndexMap[account.getName()];
+        if (index != null) {
+          backgrounds[index] = fill(new Array(maxLength), getTypeColor(account.getType()));
+        }
+      }
+      selectedRange.setBackgrounds(backgrounds);
+    }
     return false;
+  }
+
+  function fill(array: any[], value: string): any[] {
+    for (let i = 0; i < array.length; i++) {
+      array[i] = value;
+    }
+    return array;
+  }
+
+  function initilizeMatrix(matrix: any[], columns: number): any[] {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i] = new Array(columns);
+    }
+    return matrix;
+  }
+
+  function getTypeColor(type: Bkper.AccountType): string {
+    if (type == BkperApp.AccountType.ASSET) {
+      return '#dfedf6';
+    }
+    if (type == BkperApp.AccountType.LIABILITY) {
+      return '#fef3d8';
+    }
+    if (type == BkperApp.AccountType.INCOMING) {
+      return '#e2f3e7';
+    }
+    return '#f6deda';
   }
 
   function isType(groupOrType: string): boolean {
