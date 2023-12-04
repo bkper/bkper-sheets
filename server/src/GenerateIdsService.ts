9beb621171e23ed9bf565d88a10a5e761e9a366e
Bruno Coelho <bbcoelho@gmail.com>
2023-12-04 13:56:17 -0300

    refact: use TransactionsHeader class

--- server/src/GenerateIdsService.ts
+++ server/src/GenerateIdsService.ts
@@ -2,40 +2,37 @@ namespace GenerateIdsService {
 
     // Generate unique IDs for non-empty rows and returns the ID column starting from 1 = column A
     export function generate(sheet: GoogleAppsScript.Spreadsheet.Sheet): number {
-        // Try to find ID column
-        const idColumn = findIdColumn(sheet);
-        if (!idColumn) {
-            throw "Error: cannot find an 'ID' column header in the first row of this sheet";
+        // Validate header
+        const header = new TransactionsHeader(sheet.getDataRange());
+        if (!header.isValid()) {
+            throw "Error: you must set a frozen header with an ID column. See https://help.bkper.com/en/articles/2569151-bkper-add-on-for-google-sheets#h_73cf0eaf6c for more information.";
         }
-        // Fill ID column with unique IDs and return idColumn index
-        return fillIdColumn(sheet, idColumn);
-    }
 
-    function findIdColumn(sheet: GoogleAppsScript.Spreadsheet.Sheet): number | undefined {
-        if (sheet.getDataRange().isBlank()) {
-            throw "Error: empty sheet";
-        }
-        let sheetValues = sheet.getDataRange().getValues();
+        // Try to find ID column
+        const columns = header.getColumns();
         let idColumn = 0;
-        let hasIdColumn = false;
-        for (let i = 0; i < sheetValues[0].length; i++) {
-            if (!hasIdColumn && `${sheetValues[0][i]}`.toLowerCase() == 'id') {
-                idColumn = i + 1;
-                hasIdColumn = true;
+        for (const column of columns) {
+            if (column.isId()) {
+                idColumn = column.getIndex() + 1;
             }
         }
-        return hasIdColumn ? idColumn : undefined;
+        if (idColumn == 0) {
+            throw "Error: could not find an ID column in the header.";
+        }
+
+        // Fill ID column with unique IDs and return idColumn index
+        return fillIdColumn(sheet, header.getRowNum() + 1, idColumn);
     }
 
     // Fill ID column with unique IDs
-    function fillIdColumn(sheet: GoogleAppsScript.Spreadsheet.Sheet, idColumn: number): number {        
-        // Sheet has an ID header but seems to have no transitions
-        const transactionsRange = sheet.getRange(2, 1, sheet.getLastRow(), sheet.getLastColumn());
+    function fillIdColumn(sheet: GoogleAppsScript.Spreadsheet.Sheet, headerRowNum: number, idColumn: number): number {      
+          // Sheet has an ID header but seems to have no transitions
+        const transactionsRange = sheet.getRange(headerRowNum, 1, sheet.getLastRow(), sheet.getLastColumn());
         if (transactionsRange.isBlank()) {
-            throw "Sheet has an 'ID' header but appears to have no transactions in the rows below it";
+            throw "Sheet appears to contain no transactions below the header";
         }
         
-        let idColumnData = sheet.getRange(2, idColumn, sheet.getLastRow(), 1).getValues();
+        let idColumnData = sheet.getRange(headerRowNum, idColumn, sheet.getLastRow(), 1).getValues();
         let idColumnNewData: any[][] = [];
         for (let i = 0; i < idColumnData.length; i++) {
             let cellValue = idColumnData[i][0];
@@ -43,7 +40,7 @@ namespace GenerateIdsService {
                 idColumnNewData.push([cellValue]);
             } else {
                 // Only generate ID to non-empty rows
-                const emptyRow = sheet.getRange(2 + i, 1, 1, sheet.getLastColumn()).isBlank();
+                const emptyRow = sheet.getRange(headerRowNum + i, 1, 1, sheet.getLastColumn()).isBlank();
                 if (!emptyRow) {
                     const incrementedTimestamp = new Date().getTime() + i;
                     idColumnNewData.push([Utilities.getUuid() + `-${incrementedTimestamp}`]);
@@ -53,7 +50,7 @@ namespace GenerateIdsService {
             }
         }
 
-        sheet.getRange(2, idColumn, idColumnNewData.length, 1).setValues(idColumnNewData).setBackground(null);
+        sheet.getRange(headerRowNum, idColumn, idColumnNewData.length, 1).setValues(idColumnNewData).setBackground(null);
 
         // Return idColumn index
         return idColumn;
